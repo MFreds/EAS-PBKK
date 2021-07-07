91b96761eaa0d1b2a34ed05e07c8e948f3d16389
MFredS <sinagafredy26@gmail.com>
2021-07-08 00:50:40 +0700

    carousel + update + delete done

--- e-shop/app/Views/admin/_partials/head.php
+++ e-shop/app/Views/admin/_partials/head.php
@@ -15,3 +15,5 @@
 
 <!-- Custom styles for this template-->
 <link href="<?php echo base_url('css/sb-admin.css') ?>" rel="stylesheet">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
